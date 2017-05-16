66ed60097cafd383524be116abf69b20c7557290
Sofian Eka Sandra <fianeka.me@gmail.com>
2017-05-16 14:40:28 +0700

    Add Lab 6
    complete practice module 6. Custom and alert dialog

--- app/src/main/java/com/example/fianxeka/pratikum/Lab6Activity.java
+++ app/src/main/java/com/example/fianxeka/pratikum/Lab6Activity.java
@@ -1,13 +1,41 @@
 package com.example.fianxeka.pratikum;
 
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class Lab6Activity extends AppCompatActivity {
+public class Lab6Activity extends AppCompatActivity implements Lab6MyDialogFragment.UserNameListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lab6);
+    }
+    @Override
+    public void onFinishUserDialog(String user) {
+        Toast.makeText(this, "Hello, " + user, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClick(View view) {
+        // close existing dialog fragments
+        FragmentManager manager = getFragmentManager();
+        Fragment frag = manager.findFragmentByTag("fragment_edit_name");
+        if (frag != null) {
+            manager.beginTransaction().remove(frag).commit();
+        }
+        switch (view.getId()) {
+            case R.id.showcustom:
+                Lab6MyDialogFragment editNameDialog = new Lab6MyDialogFragment();
+                editNameDialog.show(manager, "fragment_edit_name");
+                break;
+            case R.id.showalert:
+                Lab6MyAlertDialogFragment alertDialogFragment = new Lab6MyAlertDialogFragment();
+                alertDialogFragment.show(manager, "fragment_edit_name");
+                break;
+        }
     }
 }
